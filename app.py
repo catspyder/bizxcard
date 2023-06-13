eaca7176d02c391e3156a19d157249aa7c3083f5
JOS JOE <iamjosjoe@gmail.com>
2023-06-13 23:16:57 +0530

    starting

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import numpy as np
+import easyocr as ocr
 
 
+class Card:
+    
+    def __init__(self,img):
+        self.text=ocr.Reader(img)
 
-st.file_uploader('upload business card',type=['png', 'jpg'] )
+    
+
+
+img=np.asarray(st.file_uploader('upload business card',type=['png', 'jpg'] ))
+card= Card(img)
+st.write(card.text)
